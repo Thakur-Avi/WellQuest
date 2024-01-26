a2d4271a5bced22cff8d31f210cbfd9910d99202
Thakur-Avi <69363644+Thakur-Avi@users.noreply.github.com>
2024-01-26 23:48:34 +0530

    Update settings.py

--- wellQuest/wellQuest/settings.py
+++ wellQuest/wellQuest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-59jj=5q81_j(t&=vfbc@35*%&17c=tn%!xdixma3$5=kmm50fl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
